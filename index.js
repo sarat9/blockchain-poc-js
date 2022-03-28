b298a69293ec14449a0bf64e1873dfa210ec8052
sarat9 <sarat.e99@gmail.com>
2022-03-28 23:34:31 +0530

    add transactions

--- index.js
+++ index.js
@@ -40,11 +40,13 @@ class Block {
 class Blockchain {
     constructor(){
         this.chain = [this.createGenesisBlock()];
+        this.pendingTrasactions = []
+        this.minerRewards = 20 //reward a miner will get if mannges to mine the transaction
     }
 
     createGenesisBlock(){
         //The Genesis Block, or Block 0, is the very first block upon which additional blocks in a blockchain are added. 
-        return new Block('14/07/1993', "Genisis Block", "0")
+        return new Block('14/07/1993', ["Genisis Block"], "0")
     }
 
     getLatestBlock(){
@@ -55,6 +57,42 @@ class Blockchain {
         newBlock.previousHash = this.getLatestBlock().hash;
         newBlock.mineBlock(MINE_DIFFICULTY)
         this.chain.push(newBlock)
+    }
+
+    minePendingTransactions(minerWalletAddress){
+        let block = new Block(Date.now(), this.pendingTrasactions)
+        block.mineBlock(MINE_DIFFICULTY)
+        console.log('Block is mined succesfully!')
+        this.chain.push(block)
+
+        this.pendingTrasactions = []
+
+        // Rewarding the miner for mining the block succesfully.
+        // So adding the reward to pending transaction
+        let rewardMinerTransaction = new Transaction(null, minerWalletAddress, this.minerRewards)
+        this.pendingTrasactions.push(rewardMinerTransaction)
+    }
+
+    createTransaction(newTransaction){
+        this.pendingTrasactions.push(newTransaction)
+    }
+
+    getBalanceOfWalletAddress(address){
+        // Get wallet balance of adress
+        let balance = 0
+
+        for(let block of this.chain){
+            for(let transaction of block.transactions){
+                if(transaction.toAddress == address){
+                    balance = balance + transaction.amount
+                }
+                if(transaction.fromAddress == address){
+                    balance = balance - transaction.amount
+                }
+            }
+        }
+        console.log('Wallet balance of address : '+address+' is amount : '+ balance)
+        return balance
     }
 
     isChainValid(){
@@ -73,16 +111,43 @@ class Blockchain {
 
 
 let saratCoin = new Blockchain()
-saratCoin.addBlock(new Block("14/07/1996", {amount: 7}))
-saratCoin.addBlock(new Block("14/07/1997", {amount: 10}))
+
+
+// ----- Playing with Blocks -----
+
+// Adding blocks with each transaction
+saratCoin.addBlock(new Block("14/07/1996", [new Transaction('my-address', 'your-address', 100)]))
+saratCoin.addBlock(new Block("14/07/1997", [new Transaction('your-address', 'my-address', 100)]))
 
 console.log(JSON.stringify(saratCoin,null,4))
 
+
+
+// ----- Playing with Transactions -----
+
+let minerWalletAddress = 'random-miner-wallet-address'
+saratCoin.createTransaction(new Transaction('address1', 'address2', 100))
+saratCoin.createTransaction(new Transaction('address1', 'address2', 100))
+saratCoin.createTransaction(new Transaction('address2', 'address1', 50))
+
+// mining all transactions first time
+saratCoin.minePendingTransactions(minerWalletAddress)
+
+console.log(JSON.stringify(saratCoin,null,4))
+
+// mining all transactions second time to get minder address also
+saratCoin.minePendingTransactions(minerWalletAddress)
+//Miner is rewarded the miner reward and is recorded in the transaction
+console.log('Wallet Balance of the miner', saratCoin.getBalanceOfWalletAddress(minerWalletAddress))
+
+
+
+
 console.log('Is BlockChain valid? : ',saratCoin.isChainValid())
 
-// trying to tamper
-console.log('trying to tamper Coin')
-saratCoin.chain[1] = new Block("14/07/1996", {amount: 1000})
+// Trying to Tamper
+console.log('Trying to Tamper Coin')
+saratCoin.chain[1] = new Block("14/07/1996", [new Transaction('my-address', 'your-address', 700)])
 saratCoin.chain[1].mineBlock(MINE_DIFFICULTY)
 // It will return false because the manipulation in transactions will result to mismatch of hash generated
 console.log('Is BlockChain valid? : ',saratCoin.isChainValid())
